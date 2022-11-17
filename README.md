b2761b909d2fc32309f2535be55292947f562611
Shalayiding Aierken <salayidin103@gmail.com>
2022-11-16 21:31:55 -0500

    basic setup for opencv

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # AsciiConverter
+
+## Environments :
+Home brew:
+```
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install HEAD/install.sh)"
+```
+Opencv :
+```
+brew install opencv
+```
+Cmake :
+```
+brew install cmake
+```
--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
+
+// standard lib
 #include<iostream>
-
-int main(){
-
+#include <fstream>
 
 
+// opencv2 lib
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    if (argc != 2) {
+        cout << "File not valid" << endl;
+        return -1;
+    }
+    
+    cv::Mat img = cv::imread(argv[1]);
+    
+    if (img.empty()) {
+        cout << "Not a valid image file" << endl;
+        return -1;
+    }
+    
+    cv::namedWindow("Image", cv::WINDOW_AUTOSIZE);
+    cv::imshow(argv[1], img);
+    
+    cv::waitKey(0);
+    cv::destroyAllWindows();
 
     return 0;
 }
